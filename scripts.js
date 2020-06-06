eb64431d273c44259030273e6f46db37e99f30b0
MadModd <66334384+MadModd@users.noreply.github.com>
2020-06-07 00:13:53 +0200

    solution

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
-let names = ["Zoli", "Ákos", "Zelma", "Tünde", "Fanny", "Peti"];
-names.forEach((name) => {
-    $('#names').append('<li>' + name + '</li>'));
+let names = ["Zoli", "Regina", "Fanny", "Béla", "Zsuzsi", "Ákos"]
+names.forEach(function (name) {
+    $('.names').append('<li>' + name + '</li>');
+});
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
 };
+
+$('body').append(
+    '<h2>' + additionalBlock.title + '</h2>',
+    '<p>' + additionalBlock.text + '</p>',
+);
+
